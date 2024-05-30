51c26871571497407dc7b74299fb5b9d94109737
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-05-30 16:03:42 +0100

    mega-menu done

--- header.js
+++ header.js
@@ -9,7 +9,7 @@ const body = document.querySelector("body");
 const mobileMenu = () => {
    menu.classList.toggle("is-active");
    menuLinks.classList.toggle("active");
-   // body.classList.toggle("active");
+   body.classList.toggle("active");
 };
 
 menu.addEventListener("click", mobileMenu);
